665700a6c0f89d708edcf652b7b60512436bca89
peageon <jinsoo2005@gmail.com>
2024-04-26 17:57:49 +0900

    fix: fixed brand logo showing on browser tab
    I don't know why but leaving favicon as favicon.ico will not work. I had to rename it to logo.ico.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Greeting",
   description: "This is a clone of Greeting",
+  icons: {
+    icon: "/logo.ico",
+  },
 };
 
 export default function RootLayout({
